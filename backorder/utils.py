eed6206fbf51bfed7b6645f8015211b24af30534
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-04-23 11:51:00 +0530

    Modified utils.py

--- backorder/utils.py
+++ backorder/utils.py
@@ -50,7 +50,7 @@ def read_dataset(fp: Path) -> DataFrame:
     # Extract pandas attribute from file extension
     suffix = fp.suffix[1:]
 
-    # Display and log the warning
+    # Print and log the warning
     if suffix not in ['csv', 'parquet']:
         warn_msg = 'utils.read_dataset: Supports CSV and parquet files easily.'
         warn(warn_msg)
@@ -63,36 +63,26 @@ def read_dataset(fp: Path) -> DataFrame:
 
 @exception_wrapper
 def to_yaml(fp: Path, data: dict):
-    """ Function for Data Validation process. """
     fp.parent.mkdir(exist_ok=True)
-
     with open(fp, 'w') as f:
         yaml.dump(data, f)
 
 
 @exception_wrapper
 def dump_object(fp: Path, obj: object) -> None:
-    """ Function for Data Transformation process. """
-    try:
-        logging.info('Enter in the save_object function of utils.')
-        fp.parent.mkdir(parents=True, exist_ok=True)
-        with open(fp, 'wb') as f:
-            dill.dump(obj, f)
-        logging.info('Exit from save_object function of utils.')
-    except Exception as e:
-        raise CustomException(e, exc_info()) from e
+    logging.info('Enter in the save_object function of utils.')
+    fp.parent.mkdir(parents=True, exist_ok=True)
+    with open(fp, 'wb') as f:
+        dill.dump(obj, f)
+    logging.info('Exit from save_object function of utils.')
 
 
 @exception_wrapper
 def load_object(fp: Path):
-    """ Function for Data Transformation process. """
-    try:
-        if not fp.exists():
-            raise FileNotFoundError(fp)
-        with open(fp, 'rb') as f:
-            return dill.load(f)
-    except Exception as e:
-        raise CustomException(e, exc_info()) from e
+    if not fp.exists():
+        raise FileNotFoundError(fp)
+    with open(fp, 'rb') as f:
+        return dill.load(f)
 
 
 @exception_wrapper
